b1d18450d82ce7e375ec733ed9409e06068826b1
sanchez.migben@gmail.com <sanchez.migben@gmail.com>
2025-10-10 13:00:57 +0200

    todo setup hasta NewCollections (inc)(01:05:33)

---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Popular.css"
+
+const Popular = () => {
+  return (
+    <div className='Popular'>
+        Popular
+    </div>
+  )
+}
+
+export default Popular
