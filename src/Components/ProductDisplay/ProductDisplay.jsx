1639215e9c2f0765cb73d277333d4009a8c960c1
sanchez.migben@gmail.com <sanchez.migben@gmail.com>
2025-10-15 17:47:28 +0200

    Hasta 2:23:24 - Modificar ShopContext

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProductDisplay.css'
+
+const ProductDisplay = (props) => {
+  return (
+    <div className='productdisplay'>
+        ProductDisplay
+    </div>
+  )
+}
+
+export default ProductDisplay
