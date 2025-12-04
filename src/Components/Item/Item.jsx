a270423dc5b09ecef38c37713848e0644a0efd83
carpen06 <carlostorrespe06@gmail.com>
2025-12-04 20:00:40 +0100

    Carlos - RelatedProducts JSX

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Item.css"
+import { Link } from 'react-router-dom'
 
 const Item = (props) => {
     return (
         <div className='item'>
+            <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)}></img></Link>
             <img src={props.image} alt="" />
             <p>{props.name}</p>
             <div className="item-prices">
