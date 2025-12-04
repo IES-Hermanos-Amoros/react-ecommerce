023e49af8b7028b509bc8a4f8d6f33a2ab1bb5d8
Jerónimo Gascón Sánchez <jergassan@alu.edu.gva.es>
2025-12-04 19:13:09 +0100

    Función de useContext en ProductDisplay

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import './ProductDisplay.css'
-import star_icon from "../Assets/star_icon.png"
-import star_dull_icon from "../Assets/star_dull_icon.png"
+import React, { useContext } from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
-  const {product} = props
+  const { product } = props;
+  const { addToCart } = useContext(ShopContext);
+
   return (
-    <div className='productdisplay'>
+    <div className="productdisplay">
       <div className="productdisplay-left">
         <div className="productdisplay-img-list">
           <img src={product.image} alt="" />
@@ -15,7 +18,7 @@ const ProductDisplay = (props) => {
           <img src={product.image} alt="" />
         </div>
         <div className="productdisplay-img">
-          <img className='productdisplay-main-img' src={product.image} alt="" />
+          <img className="productdisplay-main-img" src={product.image} alt="" />
         </div>
       </div>
       <div className="productdisplay-right">
@@ -29,8 +32,12 @@ const ProductDisplay = (props) => {
           <p>(122)</p>
         </div>
         <div className="productdisplay-right-prices">
-          <div className="productdisplay-right-price-old">${product.old_price}</div>
-          <div className="productdisplay-right-price-new">${product.new_price}</div>
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">
+            ${product.new_price}
+          </div>
         </div>
         <div className="productdisplay-right-description">
           A lightweight, usually knitted, pullover shirt, close-fitting and with
@@ -47,12 +54,22 @@ const ProductDisplay = (props) => {
             <div>XXl</div>
           </div>
         </div>
-        <button>ADD TO CART</button>
-        <p className='productdisplay-right-category'><span>Category :</span>Women , T-Shirt, Crop Top</p>
-        <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
+        <button
+          onClick={() => {
+            addToCart(product);
+          }}
+        >
+          ADD TO CART
+        </button>
+        <p className="productdisplay-right-category">
+          <span>Category :</span>Women , T-Shirt, Crop Top
+        </p>
+        <p className="productdisplay-right-category">
+          <span>Tags :</span>Modern, Latest
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDisplay
+export default ProductDisplay;
